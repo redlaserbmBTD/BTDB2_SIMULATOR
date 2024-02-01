63c3103077ef56ad0c8e2078e4561034cf95c8e2
redlaserbmBTD <biggamerer@gmail.com>
2024-02-01 18:26:56 -0500

    bank experiment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='b2sim',
-   version='1.2.6',
+   version='1.2.7',
    description='A module to simulate the generation of eco and farms in Bloons TD Battles 2',
    long_description='TODO',
    author='redlaserbm',
